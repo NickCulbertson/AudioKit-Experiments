6a96106a738465c34cb156aa01fd25684466747d
NickCulbertson <nickculbertson@gmail.com>
2023-11-06 09:08:45 -0500

    update spritekit example

--- AudioKit-Experiments/AudioKit-Experiments/Examples/SpriteSound.swift
+++ AudioKit-Experiments/AudioKit-Experiments/Examples/SpriteSound.swift
@@ -9,45 +9,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.contactDelegate = self
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         self.backgroundColor = .white
-        
-        let plat1 = SKShapeNode(rectOf: CGSize(width: 80, height: 10))
-        plat1.fillColor = .lightGray
-        plat1.strokeColor = .lightGray
-        plat1.zRotation = -.pi / 8
-        plat1.position = CGPoint(x:490,y:540+75)
-        plat1.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 80, height: 10))
-        plat1.physicsBody?.categoryBitMask = 2
-        plat1.physicsBody?.contactTestBitMask = 2
-        plat1.physicsBody?.affectedByGravity = false
-        plat1.physicsBody?.isDynamic = false
-        plat1.name = "platform1"
-        addChild(plat1)
-        
-        let plat2 = SKShapeNode(rectOf: CGSize(width: 80, height: 10))
-        plat2.fillColor = .lightGray
-        plat2.strokeColor = .lightGray
-        plat2.zRotation = .pi / 8
-        plat2.position = CGPoint(x:590,y:540)
-        plat2.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 80, height: 10))
-        plat2.physicsBody?.categoryBitMask = 2
-        plat2.physicsBody?.contactTestBitMask = 2
-        plat2.physicsBody?.affectedByGravity = false
-        plat2.physicsBody?.isDynamic = false
-        plat2.name = "platform2"
-        addChild(plat2)
-        
-        let plat3 = SKShapeNode(rectOf: CGSize(width: 80, height: 10))
-        plat3.fillColor = .lightGray
-        plat3.strokeColor = .lightGray
-        plat3.zRotation = -.pi / 8
-        plat3.position = CGPoint(x:490,y:540-75)
-        plat3.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 80, height: 10))
-        plat3.physicsBody?.categoryBitMask = 2
-        plat3.physicsBody?.contactTestBitMask = 2
-        plat3.physicsBody?.affectedByGravity = false
-        plat3.physicsBody?.isDynamic = false
-        plat3.name = "platform3"
-        addChild(plat3)
+        for i in 1...3 {
+            let plat = SKShapeNode(rectOf: CGSize(width: 80, height: 10))
+            plat.fillColor = .lightGray
+            plat.strokeColor = .lightGray
+            if i == 2 {
+                plat.zRotation = .pi / 8
+                plat.position = CGPoint(x:590,y:700-75*i)
+            } else {
+                plat.zRotation = -.pi / 8
+                plat.position = CGPoint(x:490,y:700-75*i)
+            }
+            plat.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 80, height: 10))
+            plat.physicsBody?.categoryBitMask = 2
+            plat.physicsBody?.contactTestBitMask = 2
+            plat.physicsBody?.affectedByGravity = false
+            plat.physicsBody?.isDynamic = false
+            plat.name = "platform\(i)"
+            addChild(plat)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -84,9 +64,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 self.conductor!.instrument.stop(noteNumber: MIDINoteNumber(67), channel: 0)
             }
-        }  else if contact.bodyB.node?.name == "ball" && contact.bodyA.node?.name == "ball" {
-            //balls hit
-        }else{
+        } else if contact.bodyB.node?.name != "ball" || contact.bodyA.node?.name != "ball" {
             contact.bodyB.node?.removeFromParent()
         }
     }
@@ -97,20 +75,12 @@ class SpriteSoundViewConductor: ObservableObject, HasAudioEngine {
     @Published var instrument = MIDISampler(name: "Instrument 1")
     init() {
         engine.output = Reverb(instrument)
-        try! instrument.samplerUnit.loadSoundBankInstrument(
-            at: Bundle.main.url(forResource: "Sounds/PianoMuted", withExtension: "sf2")!,
-            program: MIDIByte(0),
-            bankMSB: MIDIByte(kAUSampler_DefaultMelodicBankMSB),
-            bankLSB: MIDIByte(kAUSampler_DefaultBankLSB)
-        )
+        try! instrument.loadMelodicSoundFont(url: Bundle.main.url(forResource: "Sounds/PianoMuted", withExtension: "sf2")!, preset: 0)
     }
 }
 
 struct SpriteSoundView: View {
     @StateObject var conductor = SpriteSoundViewConductor()
-    let screenWidth  = UIScreen.main.bounds.width
-    let screenHeight = UIScreen.main.bounds.height
-    
     var scene: SKScene {
         let scene = GameScene()
         scene.size = CGSize(width: 1080, height: 1080)
@@ -118,17 +88,12 @@ struct SpriteSoundView: View {
         scene.conductor = conductor
         return scene
     }
-
     var body: some View {
         VStack {
-            SpriteView(scene: scene)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .ignoresSafeArea()
-        }
-        .onAppear {
+            SpriteView(scene: scene).frame(maxWidth: .infinity, maxHeight: .infinity).ignoresSafeArea()
+        }.onAppear {
             conductor.start()
-        }
-        .onDisappear {
+        }.onDisappear {
             conductor.stop()
         }
     }
